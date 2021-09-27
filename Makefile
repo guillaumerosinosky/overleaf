481a0151cd087a698d9ec0326e4d2ab734508e24
Guillaume Rosinosky <guillaumerosinosky@yahoo.fr>
2021-09-27 15:36:38 +0200

    add possibility to create users with password + better makefile

--- Makefile
+++ Makefile
@@ -4,9 +4,15 @@ clean:
 rm: 
 	sudo rm -Rf ~/mongo_data
 	sudo rm -Rf ~/sharelatex_data/
+	docker-compose -f ./docker-compose_ms.yml rm -f
 
 build:
-	docker-compose -f ./docker-compose_ms.yml build
+	MONOREPO_REVISION=$(git rev-parse HEAD)
+	docker-compose -f ./docker-compose_ms.yml build --build-arg MONOREPO_REVISION
 
 deploy:
 	docker-compose -f ./docker-compose_ms.yml up
+
+create-default-users:
+	docker exec overleaf_web_1 /bin/bash -c "cd /var/www/sharelatex; grunt user:create-admin --email=joe@example.com --password=password"
+	docker exec overleaf_web_1 /bin/bash -c "cd /var/www/sharelatex; grunt user:create-admin --email=bob@example.com --password=password"
--- services/web/modules/server-ce-scripts/scripts/create-user.js
+++ services/web/modules/server-ce-scripts/scripts/create-user.js
@@ -8,43 +8,62 @@ async function main() {
   const argv = minimist(process.argv.slice(2), {
     string: ['email'],
     boolean: ['admin'],
+    string: ['password'],
   })
 
-  const { admin, email } = argv
+  const { admin, email, password } = argv
   if (!email) {
     console.error(`Usage: node ${__filename} [--admin] --email=joe@example.com`)
     process.exit(1)
   }
 
   await new Promise((resolve, reject) => {
-    UserRegistrationHandler.registerNewUserAndSendActivationEmail(
-      email,
-      (error, user, setNewPasswordUrl) => {
-        if (error) {
-          return reject(error)
-        }
-        db.users.updateOne(
-          { _id: user._id },
-          { $set: { isAdmin: admin } },
-          error => {
-            if (error) {
-              return reject(error)
-            }
-
-            console.log('')
-            console.log(`\
-Successfully created ${email} as ${admin ? 'an admin' : 'a'} user.
-
-Please visit the following URL to set a password for ${email} and log in:
-
-  ${setNewPasswordUrl}
-
-`)
-            resolve()
+    if (!password) {
+      UserRegistrationHandler.registerNewUserAndSendActivationEmail(
+        email,
+        (error, user, setNewPasswordUrl) => {
+          if (error) {
+            return reject(error)
           }
-        )
+          db.users.updateOne(
+            { _id: user._id },
+            { $set: { isAdmin: admin } },
+            error => {
+              if (error) {
+                return reject(error)
+              }
+
+              console.log('')
+              console.log(`\
+  Successfully created ${email} as ${admin ? 'an admin' : 'a'} user.
+
+  Please visit the following URL to set a password for ${email} and log in:
+
+    ${setNewPasswordUrl}
+
+  `)
+              resolve()
+            }
+          )
+        }
+      )
+    } else {
+      var userDetails = {
+        email: email,
+        isAdmin: admin,
+        password: password,
       }
-    )
+      UserRegistrationHandler.registerNewUser(
+        userDetails,
+        (error, user) => {
+          if (error) {
+            return reject(error)
+          }
+          console.log(`Successfully created ${email}`)
+          resolve()
+        }
+      )
+    }
   })
 }
 
